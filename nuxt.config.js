4565b8b17cc0ce56a948020a25d07ad8a2e82c84
paul_melero <paulmelero@gmail>
2018-08-21 08:50:53 +0200

    [master] initial commit :shipit:

---
+++ nuxt.config.js
@@ -0,0 +1,84 @@
+const PurgecssPlugin = require('purgecss-webpack-plugin')
+const glob = require('glob-all')
+const path = require('path')
+
+class TailwindExtractor {
+  static extract (content) {
+    return content.match(/[A-Za-z0-9-_:\/]+/g) || []
+  }
+}
+
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    titleTemplate: '%s | graficos',
+    htmlAttrs: { lang: 'en' },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Graficos.net' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Load global CSS
+  */
+  css: ['@/assets/css/main.css'],
+  /*
+  ** This option is given directly to the vue-router Router constructor
+  */
+  router: {
+    base: '',
+    linkActiveClass: 'is-active'
+  },
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
++    ** Extract CSS
++    */
+    extractCSS: true,
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+
+      /*
+      ** Cleanup CSS with PurgeCSS
+      */
+      if (!isDev) {
+        config.plugins.push(
+          new PurgecssPlugin({
+            paths: glob.sync([
+              path.join(__dirname, './pages/**/*.vue'),
+              path.join(__dirname, './layouts/**/*.vue'),
+              path.join(__dirname, './components/**/*.vue')
+            ]),
+            extractors: [{
+              extractor: TailwindExtractor,
+              extensions: ['vue']
+            }],
+            whitelist: ['html', 'body']
+          })
+        )
+      }
+    }
+  }
+}
