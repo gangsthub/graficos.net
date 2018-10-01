705a90ab5c9bce445bd85db9f402e3dbb7337032
paul_melero <paulmelero@gmail>
2018-10-01 23:45:02 +0200

    [blog-list] updated comment

--- tailwind.config.js
+++ tailwind.config.js
@@ -255,7 +255,7 @@ module.exports = {
   textSizes: {
     'xs': '.75rem', // 12px
     'sm': '.875rem', // 14px
-    'base': '1.125rem', // 16px
+    'base': '1.125rem', // 18px
     'lg': '1.125rem', // 18px
     'xl': '1.25rem', // 20px
     '2xl': '1.5rem', // 24px
