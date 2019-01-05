d9a898509c07768621ed8b04b692d97e4ad3b254
paul_melero <paulmelero@gmail>
2019-01-05 13:08:13 +0100

    [master] corect form behav

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -7,73 +7,76 @@
       <p v-if="sent" class="text-teal-dark font-bold">🙏 Thanks for sending! I'll get in touch ASAP!</p>
     </transition>
     <form
-      @submit="onSubmit"
       name="contact"
       method="POST"
       data-netlify="true"
-      netlify
+      netlify-honeypot="bot-field"
     >
-        <label>Name
-          <input
-            type="text"
-            name="name"
-            autocomplete="name"
-            placeholder="Your name..."
-            required
-            class="
-              mb-3 mt-2
-              p-2
-              rounded
-              bg-grey-light
-              w-full
-            "
-          />
-        </label>
-        <label>Email
-          <input
-            type="email"
-            name="email"
-            autocomplete="email"
-            placeholder="Your email..."
-            required
-            class="
-              mb-3 mt-2
-              p-2
-              rounded
-              bg-grey-light
-              w-full
-            "
-          />
-        </label>
-        <label>Message:
-          <textarea
-            name="message"
-            placeholder="Your message..."
-            required
-            minlength="4"
-            class="
-              mb-3 mt-2
-              p-2
-              rounded
-              bg-grey-light
-              w-full max-w-full
-            "
-          ></textarea>
-        </label>
-        <div class="row flex justify-end">
-          <transition name="fade">
-            <button
-              type="button"
-              class="button mr-3"
-              @click="goToHome"
-              v-if="sent"
-            >Go To Home</button>
-          </transition>
-          <button type="submit" class="button"
-          >Send</button>
-        </div>
+      <p class="hidden" aria-hidden="true">
+        <label>Don’t fill this out if you're human: <input name="bot-field" type="text" /></label>
+      </p>
+      <label>Name
+        <input
+          type="text"
+          name="name"
+          autocomplete="name"
+          placeholder="Your name..."
+          required
+          class="
+            mb-3 mt-2
+            p-2
+            rounded
+            bg-grey-light
+            w-full
+          "
+        />
+      </label>
+      <label>Email
+        <input
+          type="email"
+          name="email"
+          autocomplete="email"
+          placeholder="Your email..."
+          required
+          class="
+            mb-3 mt-2
+            p-2
+            rounded
+            bg-grey-light
+            w-full
+          "
+        />
+      </label>
+      <label>Message:
+        <textarea
+          name="message"
+          placeholder="Your message..."
+          required
+          minlength="4"
+          class="
+            mb-3 mt-2
+            p-2
+            rounded
+            bg-grey-light
+            w-full max-w-full
+          "
+        ></textarea>
+      </label>
+      <div class="row flex justify-end">
+        <transition name="fade">
+          <button
+            type="button"
+            class="button mr-3"
+            @click="goToHome"
+            v-if="sent"
+          >Go To Home</button>
+        </transition>
+        <button
+          type="submit" class="button"
+          @click="onSubmit"
+        >Send</button>
+      </div>
     </form>
-    <p id="success"></p>
   </section>
 </template>
 
@@ -99,8 +102,7 @@ export default {
     }
   },
   methods: {
-    onSubmit(e) {
-      e.preventDefault()
+    onSubmit() {
       this.sent = true
     },
     goToHome() {
