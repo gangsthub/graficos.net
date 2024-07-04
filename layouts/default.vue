def9e791befa203ede176bd37753a592564cf1e5
Paul <paul.melero@backmarket.com>
2024-07-05 00:29:07 +0200

    fix stacking contexts

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="flex flex-col content-around min-h-screen bg-container relative">
+  <div class="flex flex-col content-around min-h-screen relative">
     <header role="banner" class="isolate z-1">
       <main-presentation-head-nav />
     </header>
     <main class="container mx-auto p-4 flex-grow" role="main">
-      <slot />
-      <div class="bg-container" aria-hidden="true"></div>
+      <div class="relative z-[2]"><slot /></div>
+      <div class="isolate"><div class="bg-container" aria-hidden="true"></div></div>
     </main>
     <footer role="contentinfo">
       <main-presentation-the-footer />
@@ -27,7 +27,7 @@ useHead({
 .bg-container {
   &:after {
     content: '';
-    @apply absolute inset-0 w-full h-full;
+    @apply absolute z-0 inset-0 w-full h-full;
     background-image: url('~/assets/images/valves/v2.svg');
     background-size: 100vw;
     background-repeat: no-repeat;
