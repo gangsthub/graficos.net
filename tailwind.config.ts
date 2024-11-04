f927c3b4e37c87a84bf566f028bf8808ca51e9ed
Paul <paul.melero@backmarket.com>
2024-11-04 23:11:50 +0100

    add blink and typing animations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -104,6 +104,15 @@ export default {
       zIndex: {
         '1': '1',
       },
+      animation: {
+        blink: 'blink 1s step-start infinite',
+      },
+      keyframes: {
+        blink: {
+          '0%, 100%': { opacity: 1 },
+          '50%': { opacity: 0 },
+        },
+      },
     },
   },
 }
